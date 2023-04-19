89f41303f0db8d80f0549539935ff8e9b41a88be
Andrei Tecsor <andrei-marius.tecsor@ing.com>
2023-04-20 00:54:24 +0300

    requester initial commit
    request

---
+++ src/test/java/tecsor/andrei/dissertation/requester/RequesterApplicationTests.java
@@ -0,0 +1,13 @@
+package tecsor.andrei.dissertation.requester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
